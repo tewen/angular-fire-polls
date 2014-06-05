bca9c510b9d3f9e65681cb59a58cc3c351bfbb43
Trevor Ewen <tewen@neosavvy.com>
2014-06-04 21:57:11 -0400

    Added the question type selector.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,11 @@
 
 angular.module('firePollsApp.controllers', [])
     .controller('MainCtrl', ['$scope', 'Questions', function ($scope, Questions) {
+        /* Initialization */
         Questions.sync().$bind($scope, 'questions');
+        $scope.questionTypes = ['True/False', 'Multiple Choice'];
+        $scope.selections = {selectedQuestionType: $scope.questionTypes[0]};
+
 
         /* Action Handlers */
         $scope.onSubmit = function (e) {
--- test/spec/controllers/main-spec.js
+++ test/spec/controllers/main-spec.js
@@ -2,19 +2,60 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('firePollsApp.controllers'));
+    // load the controller's module
+    beforeEach(module('firePollsApp.controllers', {
+        Questions: jasmine.createSpyObj('Questions', ['sync'])
+    }));
 
-  var MainCtrl,
-    scope;
+    var MainCtrl,
+        scope,
+        Questions,
+        $bindSpy;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope, _Questions_) {
+        scope = $rootScope.$new();
+        $bindSpy = jasmine.createSpy('$bindSpy');
+        Questions = _Questions_;
+        Questions.sync.andReturn({$bind: $bindSpy});
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope
+        });
+    }));
+
+
+    describe("Initialization", function () {
+
+        it("Should call Questions.sync", function () {
+            expect(Questions.sync).toHaveBeenCalledWith();
+        });
+
+        it("Should call the $bind on sync with $scope and questions", function () {
+            expect($bindSpy).toHaveBeenCalledWith(scope, 'questions');
+        });
+
+        it("Should throw questionTypes on the $scope", function () {
+            expect(scope.questionTypes).toEqual(['True/False', 'Multiple Choice']);
+        });
+
+        it("Should set scope.selectedQuestionType to the first of the question types", function () {
+            expect(scope.selectedQuestionType).toEqual(scope.questionTypes[0]);
+        });
     });
-  }));
 
+    describe("Action Handlers", function () {
+        describe("onSubmit", function () {
 
+        });
+
+        describe("onDelete", function () {
+            it("Should call the event.stopPropagation method", function () {
+
+            });
+
+            it("Should call the $remove method with the id on $scope.questions", function () {
+
+            });
+        });
+    });
 });
